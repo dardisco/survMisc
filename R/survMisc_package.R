4bd360486719f65bb4ceee2235bbf387fb8ff60e
dardisco <christopherdardis@gmail.com>
2014-04-08 18:18:23 -0700

    package description in .R file

---
+++ R/survMisc_package.R
@@ -0,0 +1,18 @@
+#' Miscellaneous functions for survival analysis.
+#' \tabular{ll}{
+#'  Package: \tab survMisc\cr
+#'  Type: \tab Package\cr
+#'   Version: \tab 0.41\cr
+#'  Date: \tab 2014-04-02\cr
+#' License: \tab GPL (>= 2)\cr
+#' LazyLoad: \tab yes
+#' }
+#' A collection of functions for analysis of survival data. These
+#' extend the methods available in package survival.
+#' @title Miscellaneous functions for survival analysis.
+#' @name survMisc-package
+#' @aliases survMisc
+#' @docType package
+#' @author Chris Dardis \email{christopherdardis@@gmail.com}
+#' @keywords package
+NULL
